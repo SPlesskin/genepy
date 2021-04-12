71fe90436356f85542911419d5a1cfa09eec46dc
Erwan <erwan.grace@outlook.fr>
2021-04-12 22:35:40 +0200

    Update copyright notices

--- src/exception/Exception.cpp
+++ src/exception/Exception.cpp
@@ -1,5 +1,5 @@
 /* Genepy - A General-Purpose Library
- * Copyright (C) 2020  Erwan Grâce <erwan.grace@outlook.fr>
+ * Copyright (C) 2020, 2021  Erwan Grâce <erwan.grace@outlook.fr>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
